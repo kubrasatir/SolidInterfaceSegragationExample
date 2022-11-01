e613a7c32ebdac244c85760397e2946ab947cd19
kubrasatir <kubra-satir@hotmail.com>
2022-11-01 09:13:16 +0300

    Initial commit

---
+++ SolidInterfaceSegragationExample/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using SolidInterfaceSegragationExample;
+
+IWorker contractWorker=new ContractEmployee();
+contractWorker.HourlyRate = 10;
+contractWorker.HoursInMonth = 2;
+IWorker fullTimeWorker=new FullTimeEmployee();
+fullTimeWorker.MonthlySalary = 300;
+fullTimeWorker.OtherBenefits = 40;
+
+Console.WriteLine($"contractWorker {contractWorker.CalculateWorkedSalary()}");
+Console.WriteLine($"fullTimeWorker {fullTimeWorker.CalculateNetSalary()}");
+Console.ReadKey();
